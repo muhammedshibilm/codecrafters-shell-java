55811bf58fd31133ce45abc6feebc5770a49c128
Muhammed Shibil M <muhammedshibilm321@gmail.com>
2026-07-19 23:51:08 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter command ");
         String command = sc.nextLine();
         System.out.println(command + ": command not found");
+        sc.close();
     }
 }
