965cd973011c609abd0b91e17b23b7ea28ceba6a
Muhammed Shibil M <muhammedshibilm321@gmail.com>
2026-07-20 23:26:42 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,14 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
+        String message =" is a shell buildin";
         while (true) {
         System.out.print("$ ");
         String command = sc.nextLine();
 
         if (command.equals("exit")){
-            break;
+            System.out.println(command + message);
         }else if (command.startsWith("echo ")) {
-             System.out.println(command.substring(5));
+            //  System.out.println(command.substring(5));
+            System.out.println(command+ message);
         }else
         System.out.println(command + ": command not found");
         }
