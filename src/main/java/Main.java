23cd78724942ae3a7f0115d4d66308e01595e290
Muhammed Shibil M <muhammedshibilm321@gmail.com>
2026-07-20 23:38:22 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,13 +7,13 @@ public class Main {
         while (true) {
         System.out.print("$ ");
         String command = sc.nextLine();
-
+        
         if (command.contains("exit")){
             System.out.println("exit " + message);
         }else if (command.contains("echo")) {
             //  System.out.println(command.substring(5));
             System.out.println("echo "+ message);
-        }else if ( command.contains("exit")){
+        }else if ( command.contains("type")){
                 System.out.println("type "+message);
         }else{
         System.out.println(command.split("\\s+")[1] + ": command not found");
