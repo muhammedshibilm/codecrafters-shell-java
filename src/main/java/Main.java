9987d64ea80b7770f4c77d28f3fa4e75d2a9ae7c
Muhammed Shibil M <muhammedshibilm321@gmail.com>
2026-07-21 00:03:41 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,23 +3,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        String message ="is a shell builtin";
         while (true) {
         System.out.print("$ ");
         String command = sc.nextLine();
-        
-        String[] keywords = command.trim().split("\\s+");
 
-        if (keywords[1].equals("exit")){
-            System.out.println("exit " + message);
-        }else if (keywords[1].equals("echo")) {
-            //  System.out.println(command.substring(5));
-            System.out.println("echo "+ message);
-        }else if ( keywords[1].equals( "type")){
-                System.out.println("type "+message);
+        if (command.equals("exit")){
+            break;
+        }else if (command.startsWith("echo ")) {
+             System.out.println(command.substring(5));
+        }else if (command.startsWith("type")) {
+            String[] keyword =  command.split("\\s+");
+            if (keyword[1].equals("echo") || keyword[1].equals("exit") || keyword[1].equals("type")) {
+                System.out.println(keyword[1] + " is a shell builtin");
+            }else{
+                System.out.println(keyword[1] +": not found");
+            }
         }else{
-        System.out.println(command.split("\\s+")[1] + ": not found");
+        System.out.println(command + ": command not found");
         }
-    
-} }
+     
+    }
+}
 }
